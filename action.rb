2d269639939f7c3204ba9b76668ee93153cd19ae
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2021-07-09 14:50:06 +0900

    Support EXCEPTION_LABEL

--- action.rb
+++ action.rb
@@ -2,6 +2,7 @@ require 'octokit'
 
 repo = ENV["GITHUB_REPOSITORY"]
 label = ENV["LABEL"]
+exception_label = ENV["EXCEPTION_LABEL"]
 expire_days = ENV["EXPIRE_DAYS"]
 
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
@@ -13,6 +14,9 @@ now = Time.new.to_i
 expire_days_in_seconds = expire_days.to_i * 60 * 60 * 24
 
 open_issues.each do |issue|
+  if exception_label and issue.labels.any?{|label| label.name == exception_label }
+    next
+  end
   timeline = client.issue_timeline(repo, issue.number)
   last_labeled_event = timeline.select{|event| event.event == "labeled" }.last
   past_seconds = now - last_labeled_event.created_at.to_i
